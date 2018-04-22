7cbc17940b0dd5869695d6a0487fa8fe3a14f98b
Tmsnoo <tomek.laskawiec@gmail.com>
2018-04-22 16:34:17 +0200

    switched off filter

--- src/main/java/pl/coderslab/LoggedInCheckedFilter.java
+++ src/main/java/pl/coderslab/LoggedInCheckedFilter.java
@@ -15,37 +15,38 @@ import pl.coderslab.Entity.User;
 
 // @WebFilter registers filter immediately without any other configuration
 @WebFilter(description = "Check if user is logged in and if not then redirect", urlPatterns = { "/*" })
-public class LoggedInCheckedFilter extends OncePerRequestFilter {
+public class LoggedInCheckedFilter //extends OncePerRequestFilter {
+{}
 
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-			throws ServletException, IOException {
-
-		final HttpServletRequest req = (HttpServletRequest) request;
-		final String url = req.getRequestURL().toString();
-
-		if (allowedToEveryone(url)) {
-			filterChain.doFilter(request, response);
-			return;
-		}
-
-		HttpSession sess = req.getSession();
-		User user = (User) sess.getAttribute("loggedUser");
-
-		if (user == null) {
-			HttpServletResponse httpResponse = (HttpServletResponse) response;
-			httpResponse.sendRedirect(req.getContextPath() + "/user/login");
-			return;
-		}
-
-		filterChain.doFilter(request, response);
-	}
-
-	private boolean allowedToEveryone(final String url) {
-		if (url.endsWith("/user/register") || url.endsWith("/user/login")) {
-			return true;
-		}
-
-		return false;
-	}
-}
+//	@Override
+//	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+//			throws ServletException, IOException {
+//
+//		final HttpServletRequest req = (HttpServletRequest) request;
+//		final String url = req.getRequestURL().toString();
+//
+//		if (allowedToEveryone(url)) {
+//			filterChain.doFilter(request, response);
+//			return;
+//		}
+//
+//		HttpSession sess = req.getSession();
+//		User user = (User) sess.getAttribute("loggedUser");
+//
+//		if (user == null) {
+//			HttpServletResponse httpResponse = (HttpServletResponse) response;
+//			httpResponse.sendRedirect(req.getContextPath() + "/user/login");
+//			return;
+//		}
+//
+//		filterChain.doFilter(request, response);
+//	}
+//
+//	private boolean allowedToEveryone(final String url) {
+//		if (url.endsWith("/user/register") || url.endsWith("/user/login")) {
+//			return true;
+//		}
+//
+//		return false;
+//	}
+//}
